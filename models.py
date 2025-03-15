d1d06c8c441c1756ef0dd9a3483773dae328aa97
PyMustafa <proo.eng2018@gmail.com>
2025-03-16 00:41:14 +0200

    feat(app): implement user model and enhance book form with additional fields and default image

--- models.py
+++ models.py
@@ -1,22 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
-class Author(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    books = db.relationship('Book', backref='author', lazy=True)
-
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    publish_date = db.Column(db.String(50))
-    price = db.Column(db.Float)
-    appropriate_age = db.Column(db.String(50))
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
-    image_url = db.Column(db.String(200))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,8 +12,40 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
-    def set_password(self, password):
-        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+    # flask-login integration methods
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    books = db.relationship('Book', backref='author', lazy=True)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    publish_date = db.Column(db.String(10), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    appropriate_age = db.Column(db.String(20), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    image_url = db.Column(db.String(200), nullable=False, default='uploads/default.jpg')
